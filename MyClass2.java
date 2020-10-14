51f08255169a28152f4065c37ccafd1ddc421bec
atilaufg <70610178+atilaufg@users.noreply.github.com>
2020-10-13 21:36:57 -0300

    13

---
+++ MyClass2.java
@@ -0,0 +1,8 @@
+public class MyClass2 {
+  public static void main(String[] args) {
+
+  String nome = "Atila P S A Xavier";
+    
+System.out.println(nome);
+  }
+}
---
+++ MyClass3.java
@@ -0,0 +1,12 @@
+public class MyClass3 {
+  public static void main(String[] args) {
+
+  int data = 13102020;
+  int nascimento = 1999;
+  int idade = 21;
+    
+System.out.println(data);
+System.out.println(nascimento);
+System.out.println(idade);
+  }
+}
